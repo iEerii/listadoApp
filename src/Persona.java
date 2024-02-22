cedc7d3da611ce2c582309b2147850ddd32be348
Eri <erickaponce92@gmail.com>
2024-02-22 10:08:13 -0500

    create class Persona

---
+++ src/Persona.java
@@ -0,0 +1,22 @@
+public class Persona {
+    private int id;
+    private String nombre;
+    private String telefono;
+    private String email;
+    private static int numeroPersonas = 0;
+
+    //Constructor vacio
+    public Persona(){
+        this.id = ++Persona.numeroPersonas;
+    }
+
+    //Constructor con argumentos
+    public Persona(String nombre, String telefono, String email){
+        this();
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+
+}
