e706dc23f5421d07786a3a49fe9b324e2efb2ccc
Eri <erickaponce92@gmail.com>
2024-02-22 10:40:06 -0500

    create getters and setters

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
-
-
+        Persona persona1 = new Persona("Juan Perez", "5544112233", "juan@mail.com");
+        System.out.println(persona1);
     }
 }
